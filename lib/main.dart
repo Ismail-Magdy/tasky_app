5ce3f9a6106e5620dda4b59ac6f475324c1650d8
Ismail Magdy <ismailmagdy920@gmail.com>
2026-02-11 09:45:09 +0200

    tasky_app

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import "package:flutter/material.dart";
+import "package:tasky_app/tasky_app.dart";
+
+void main() {
+  runApp(const TaskyApp());
+}
---
+++ lib/tasky_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TaskyApp extends StatelessWidget {
+  const TaskyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false);
+  }
+}
